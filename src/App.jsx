d9e291c5fc9a061f2aed2def154bf80b06c08b1a
miguelmorandini <miguelmorandini2005@gmail.com>
2025-05-01 11:30:24 -0300

    inserinddo autenticação de usuário

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,53 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+
+import { useState, useEffect } from 'react'
+import { useAuthentication } from './hooks/useAuthentication'
+
+import { AuthProvider } from './context/AuthContext'
+
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+
 import './App.css'
 
+
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <>
       <div>
-        <BrowserRouter>
-        <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+        <AuthProvider value={{ user }}>
+          <BrowserRouter>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
       </div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ const Navbar = () => {
     <>
       <nav className={styles.navbar}>
         <ul className={styles.links_list}>
-          <NavLink to="/" className={styles.brand} activeClassName={styles.active}>
-          <li><span>Life</span>Dev</li>
+          <NavLink to="/" className={styles.brand} activeclassname={styles.active}>
+            <li><span>Life</span>Dev</li>
           </NavLink>
-          <NavLink to="/login" className={styles.link} activeClassName={styles.active}>
-          <li>Login</li>
+          <NavLink to="/login" className={styles.link} activeclassname={styles.active}>
+            <li>Login</li>
           </NavLink>
-          <NavLink to="/register" className={styles.link} activeClassName={styles.active}>
-          <li>Register</li>
+          <NavLink to="/register" className={styles.link} activeclassname={styles.active}>
+            <li>Register</li>
           </NavLink>
           <button className={styles.exit}>Exit</button>
         </ul>
