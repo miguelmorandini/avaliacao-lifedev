e4b3598aadf5e1257ddc2560e52c7abdfcbf1d87
miguelmorandini <miguelmorandini2005@gmail.com>
2025-05-07 08:23:43 -0300

    Ajustes de Estéticos

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className={styles.about}>
       <h2>Sobre o Life<span>Dev</span></h2>
-      <p>Projeto criado para servir como base para a aprendizagem de React, no 3º semestre do curso de Desenvovimento de Software Multiplataforma da Fatec Matão - Luiz Marchesan</p>
+      <p>Projeto criado para servir como base para a aprendizagem de React, no 3º semestre do curso de Desenvovimento de Software Multiplataforma da Fatec Matão - Luiz Marchesan. Desenvolvido pelo aluno Miguel Morandini.</p>
       <Link to="/posts/create" className="btn">Criar Post</Link>
     </div>
   )
